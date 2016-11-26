05fde2ca6dc21ef6f459d851e00a18998b98502b
Mikko Muhonen <muhiz83@gmail.com>
2016-11-26 13:54:34 +0200

    Modified theatres -route and added cities-route - Adds /api/cities - /api/theatres now returns correct data - Adds model for Theatre

--- backend/filmfinder.py
+++ backend/filmfinder.py
@@ -1,15 +1,65 @@
 import requests
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from xml.etree import ElementTree
 
 app = Flask(__name__)
 
 
-# id, name, city, location x,y
+class Theatre:
+    def __init__(self, theatre_id, name, city):
+        self.id = theatre_id
+        self.name = name
+        self.city = city
+        # self.location x, y
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'city': self.city
+        }
+
+
+@app.route('/api/cities', methods=['GET'])
+def get_cities():
+    response = requests.get("http://www.finnkino.fi/xml/TheatreAreas/", timeout=1.000)
+
+    root = ElementTree.fromstring(response.content)
+
+    cities = {}
+
+    for theatre in root.findall('TheatreArea'):
+        area_id = int(theatre.find('ID').text)
+        area_name = theatre.find('Name').text
+        area_name = area_name.split(':')
+
+        # Remove 1029 Valitse teatteri/kaupunki and choose only cities
+        if not area_id == 1029 and len(area_name) == 1:
+            cities[area_id] = area_name[0]
+
+    return jsonify(cities)
+
+
+# returns Theatre -list.
 @app.route('/api/theatres', methods=['GET'])
 def get_theatres():
-    r = requests.get("http://www.finnkino.fi/xml/TheatreAreas/")
-    return r.text
+    response = requests.get("http://www.finnkino.fi/xml/TheatreAreas/",  timeout=1.000)
+
+    root = ElementTree.fromstring(response.content)
+
+    theatres = []
+
+    for theatre in root.findall('TheatreArea'):
+        area_id = int(theatre.find('ID').text)
+        area_name = theatre.find('Name').text
+        area_name = area_name.split(':')
+
+        # Remove 1029 Valitse teatteri/kaupunki and choose only cities
+        if not area_id == 1029 and len(area_name) > 1:
+            area_name[1] = area_name[1].lstrip()
+            theatres.append(Theatre(area_id, area_name[1], area_name[0]))
+
+    return jsonify({'theatres': [t.serialize() for t in theatres]})
 
 
 # Movie title, Genres, Description, Runtime, Show starts, IMDb rating
