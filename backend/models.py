e84120a58874818b35f2f50059c936db64d3602b
Mikko Muhonen <muhiz83@gmail.com>
2016-12-04 16:35:10 +0200

    Cleans code, adds location and ratings - Moves classes to Models - Adds location to Theatres - Adds ratings to Shows

---
+++ backend/models.py
@@ -0,0 +1,38 @@
+
+class Theatre:
+    def __init__(self, theatre_id, name, city, location):
+        self.id = theatre_id
+        self.name = name
+        self.city = city
+        self.location = location
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'city': self.city,
+            'location': self.location
+        }
+
+
+# Movie title, Genres, Description, Runtime, Show starts, IMDb rating
+class Show:
+    def __init__(self, show_id, title, genre, description, length, starttime, rating):
+        self.show_id = show_id
+        self.title = title
+        self.genre = genre
+        self.description = description
+        self.length = length
+        self.starttime = starttime
+        self.rating = rating
+
+    def serialize(self):
+        return {
+            'id': self.show_id,
+            'title': self.title,
+            'genre': self.genre,
+            'description': self.description,
+            'runtime': self.length,
+            'start_time': self.starttime,
+            'rating': self.rating
+        }
