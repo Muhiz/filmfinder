4027385ac77e611a034ca9d5d7960d62752ffd5f
Mikko Muhonen <muhiz83@gmail.com>
2016-12-11 17:28:34 +0200

    Improves OMDB hit rate - Removes extra info from movie name when trying to search from OMDB - Retry with simpler name in case first time fails

--- backend/omdbapi.py
+++ backend/omdbapi.py
@@ -1,10 +1,39 @@
 import requests
 
 
+def sanitize_movie_name(name):
+    """Removes any extra info from movie name added by Finnkino"""
+    name = name.rstrip()
+    # Remove dub and 2D / 3D from the end of name and remove trailing space
+    if name.endswith('(dub)'):
+        name = name[:-5].rstrip()
+
+    if name.endswith('(2D)') or name.endswith('(3D)'):
+        name = name[:-4].rstrip()
+
+    if name.endswith('2D') or name.endswith('3D'):
+        name = name[:-2].rstrip()
+
+    print(name)
+    return name
+
+
+def query_movie_rating(name):
+    """Queries movie rating form OMDB API"""
+    query = {'t': name, 'y': 'plot', 'r': 'json'}
+    return requests.get("http://www.omdbapi.com/", params=query).json()
+
+
 def fetch_movie_rating(name):
     """Fetches movie rating from OMDB API"""
-    query = {'t': name, 'y': 'plot', 'r': 'json'}
-    r = requests.get("http://www.omdbapi.com/", params=query).json()
+    name = sanitize_movie_name(name)
+    r = query_movie_rating(name)
+
+    if r['Response'] == 'False':
+        # Try again by splitting extra part out
+        print('Not found')
+        r = query_movie_rating(name.split('-')[0])
+        print(name.split('-')[0])
 
     if 'imdbRating' in r:
         rating = r['imdbRating']
